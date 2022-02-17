fdc89c11394b2736e42c88c466b8c08ba65f16f1
rupesss-09 <rupesss09@gmail.com>
2022-02-16 21:32:59 -0500

    Adding logic if authentication then only render logout button else render login button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Logout = () => {
-    const { logout } = useAuth0();
+    const { logout, isAuthenticated } = useAuth0();
 
-    return <button onClick={() => logout()}>Log Out</button>;
+    return isAuthenticated && <button onClick={() => logout()}>Log Out</button>;
 };
 
 export default Logout;
