e189df96eee573ed8709a9ee3f0aff5a34d78e68
rupesss-09 <rupesss09@gmail.com>
2022-02-16 21:33:39 -0500

    Adding logic when authentication is loading then show loading instead of login button

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+    const { isLoading } = useAuth0();
+    if (isLoading) return <div>Loading...</div>;
     return (
         <>
             <Login />
