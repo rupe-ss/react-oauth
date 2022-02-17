332a787bcef067ba966dbfb88d65c854a56cddf1
rupesss-09 <rupesss09@gmail.com>
2022-02-16 20:40:34 -0500

    Added package "yarn add @auth0/auth0-react"

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const client = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+//Below window.location.origin will redirect us to same location where login page was created
 ReactDOM.render(
-    <React.StrictMode>
+    <Auth0Provider
+        domain={domain}
+        clientId={client}
+        redirectUri={window.location.origin}>
         <App />
-    </React.StrictMode>,
+    </Auth0Provider>,
     document.getElementById('root')
 );
